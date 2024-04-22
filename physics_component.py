63e203dc23eb10b58dd2e8d75fdec8eb909c007f
StellaWaa <stella989@protonmail.com>
2024-04-22 14:33:46 +0200

    physics testing branch

--- physics_component.py
+++ physics_component.py
@@ -1,4 +1,13 @@
-
+import pygame
+vector2d = pygame.math.Vector2
 class PhysicsComponent:
     def __init__(self):
+        self.speed = vector2d(0,0)
+        self.accel = vector2d(0,0)
+        self.friction = vector2d(0,0)
         pass
+    def check_colision(self):
+        pass
+    def move(self, entity, move_vec):
+        pass
+        
