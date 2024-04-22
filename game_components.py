c42706cdee8004f1067198592d8e7bc80ca759c6
StellaWaa <stella989@protonmail.com>
2024-04-23 00:08:43 +0200

    put input logic into input component

--- game_components.py
+++ game_components.py
@@ -13,13 +13,9 @@ class PhysicsComponent:
     def check_colision(self, other_entities):
         pass
     def move(self, move_vec, other_entities = None):
+        self.accel.x = self.def_speed*move_vec.x
         pass
     def update_pos(self, other_entities = None):
-        key = pygame.key.get_pressed()
-        if key[K_LEFT] or key[K_a]:
-            self.accel.x = -self.def_speed
-        if key[K_RIGHT] or key[K_d]:
-             self.accel.x = self.def_speed  
         # tarcie powoduje hamowanie bo acc bedzie ujemne caly czas (dodatnie tylko w chwili nacisniecia klawisza)
         self.accel -= self.friction*self.speed
          # reszta to fizyka lore
@@ -30,16 +26,12 @@ class PhysicsComponent:
 class InputComponent:
     def __init__(self) -> None:
         self.move_direction = vector2d(0,0)
-    def get_player_movement_vec(self) -> vector2d:
+    def get_movement_vec(self):
+        direction = vector2d(0,0)
         key = pygame.key.get_pressed()
         if key[K_LEFT] or key[K_a]:
-            self.accel.x = -self.def_speed
+            direction.x = -1
         if key[K_RIGHT] or key[K_d]:
-             self.accel.x = self.def_speed  
-        # tarcie powoduje hamowanie bo acc bedzie ujemne caly czas (dodatnie tylko w chwili nacisniecia klawisza)
-        self.accel -= self.friction*self.speed
-         # reszta to fizyka lore
-        self.speed += self.accel
-        self.entity.pos += self.speed + self.accel/2
-        self.entity.shape.midbottom = self.pos
+             direction.x = 1
+        return direction
         
