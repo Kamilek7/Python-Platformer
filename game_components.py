8b229a7c95a935def15b93733e5c58e9ae8b082d
StellaWaa <stella989@protonmail.com>
2024-04-22 14:42:34 +0200

    file_update

--- game_components.py
+++ game_components.py
@@ -1,9 +1,10 @@
 import pygame
+from entities import Entity
 vector2d = pygame.math.Vector2
 class PhysicsComponent:
     def __init__(self):
         self.speed = vector2d(0,0)
-        self.accel = vector2d(0,0)
+        self.accel = vector2d(0,-5)
         self.friction = vector2d(0,0)
         pass
     def check_colision(self):
