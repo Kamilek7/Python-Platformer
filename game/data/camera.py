25cc8a279f5d20e34e4807a23bf10ba1ef680048
Stella <stella989@protonmail.com>
2024-04-25 17:17:54 +0200

    camera comments

--- game/data/camera.py
+++ game/data/camera.py
@@ -6,12 +6,14 @@ class Camera:
         self.other_objects = other_objects
     def update(self):
         #run every tick
+        #moves the camera by changing pos of all objects
         move_vec = -self.focus_object.last_movement
         self.focus_object.move_by(move_vec)
         for entity in self.other_objects:
             entity.move_by(vector2d((move_vec.x), (move_vec.y)))
             print(entity.pos)
     def centre_camera(self, window_dimensions):
+        #centers the camera to the middle of window_dimensions
         window_centre = window_dimensions/2
 
         move_vec = window_centre - self.focus_object.pos
