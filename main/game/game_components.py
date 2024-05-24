2a04d2e55c6be10ee726290931cb23f1836e7baa
kamilek7 <kamijar20031@gmail.com>
2024-05-24 19:19:28 +0200

    poprawka dla background i kolizji

--- main/game/game_components.py
+++ main/game/game_components.py
@@ -19,14 +19,10 @@ class TextureComponent:
     spritesB = pygame.sprite.Group()
     spritesF = pygame.sprite.Group()
     messageBoxes = []
-    background=None
-    fadedBackground = None
-    changeFlag = False
-    fadeFlag = False
-    alpha = 255
-    backarea = None
-    backareaTemp = None
-    backshape = None
+    backgrounds = []
+    tempBG2 = None
+    tempBG1 = None
+    backgroundAlpha = 0
     messageFadeFlag = False
     messageVisibility = False
     messageLifespan = 0
@@ -36,47 +32,40 @@ class TextureComponent:
     currentBGsize = vector2d((0,0))
     @staticmethod
     def scaleBackground(window):
-        TextureComponent.backarea = pygame.transform.scale(TextureComponent.backarea,(window.get_width(),window.get_height()))
+        TextureComponent.tempBG1 = pygame.transform.scale(TextureComponent.tempBG1,(window.get_width(),window.get_height()))
     def manageBackground(window):
-        if TextureComponent.backarea==None:
-            TextureComponent.backarea = pygame.Surface((window.get_width(),window.get_height()), pygame.SRCALPHA, 32)
-            TextureComponent.backareaTemp = pygame.Surface((window.get_width(),window.get_height()), pygame.SRCALPHA, 32)
-            TextureComponent.backshape = TextureComponent.backarea.get_rect(center = (0,0))
-            backpos = vector2d((0,0))
-            TextureComponent.backshape.topleft = backpos
-        if TextureComponent.changeFlag:
-            TextureComponent.changeFlag = False
-            TextureComponent.alpha = 255
-            if TextureComponent.fadedBackground != None:
-                TextureComponent.fadeFlag = True
-                TextureComponent.backareaTemp = pygame.image.load(path.join(BACKGROUNDS_DIR,TextureComponent.fadedBackground)).convert()
-                TextureComponent.backareaTemp = pygame.transform.scale(TextureComponent.backarea,(window.get_width(),window.get_height()))
-                TextureComponent.alpha=0
-                TextureComponent.backareaTemp.set_alpha(255)
-                window.blit(TextureComponent.backareaTemp, TextureComponent.backshape)
-            TextureComponent.backarea = pygame.image.load(path.join(BACKGROUNDS_DIR,TextureComponent.background)).convert()
-            TextureComponent.backarea = pygame.transform.scale(TextureComponent.backarea,(window.get_width(),window.get_height()))
-            TextureComponent.backarea.set_alpha(TextureComponent.alpha)
-        elif TextureComponent.fadeFlag:
-            if TextureComponent.alpha<255:
-                TextureComponent.alpha+=10
-                TextureComponent.backarea.set_alpha(TextureComponent.alpha)
-                window.blit(TextureComponent.backareaTemp, TextureComponent.backshape)
+        if len(TextureComponent.backgrounds)==2:
+            if TextureComponent.tempBG2==None:
+                TextureComponent.tempBG2 = pygame.image.load(path.join(BACKGROUNDS_DIR, TextureComponent.backgrounds[1])).convert_alpha()
+                TextureComponent.tempBG2 = pygame.transform.scale(TextureComponent.tempBG2,(window.get_width(),window.get_height()))
+            window.blit(TextureComponent.tempBG1,(0, 0))
+            window.blit(TextureComponent.tempBG2,(0, 0))
+            if TextureComponent.backgroundAlpha<=255:
+                TextureComponent.backgroundAlpha+=10
+                TextureComponent.tempBG2.set_alpha(TextureComponent.backgroundAlpha)
             else:
-                TextureComponent.alpha=255
-                TextureComponent.fadeFlag=False
-                TextureComponent.backarea.set_alpha(TextureComponent.alpha)
-        window.blit(TextureComponent.backarea, TextureComponent.backshape)
+                TextureComponent.backgroundAlpha=0
+                TextureComponent.tempBG1 = pygame.image.load(path.join(BACKGROUNDS_DIR, TextureComponent.backgrounds[1])).convert_alpha()
+                TextureComponent.tempBG1 = pygame.transform.scale(TextureComponent.tempBG1,(window.get_width(),window.get_height()))
+                TextureComponent.backgrounds.remove(TextureComponent.backgrounds[0])
+                TextureComponent.tempBG2 = None
+        elif len(TextureComponent.backgrounds)==1:
+            if TextureComponent.tempBG1==None:
+                TextureComponent.tempBG1 = pygame.image.load(path.join(BACKGROUNDS_DIR, TextureComponent.backgrounds[0])).convert_alpha()
+                TextureComponent.tempBG1 = pygame.transform.scale(TextureComponent.tempBG1,(window.get_width(),window.get_height()))
+            window.blit(TextureComponent.tempBG1,(0, 0))
+        
+
     def changeBackground(newBackground):
         TextureComponent.currentBGcoords.x = newBackground.pos.x - 40
         TextureComponent.currentBGcoords.y = newBackground.pos.y - 40
         TextureComponent.currentBGsize.x = newBackground.get_width()+40
         TextureComponent.currentBGsize.y = newBackground.get_height()
-        newBackground = newBackground.background
-        if TextureComponent.background!=newBackground:
-            TextureComponent.fadedBackground=TextureComponent.background
-            TextureComponent.background = newBackground
-            TextureComponent.changeFlag = True
+        checkLengthLess = len(TextureComponent.backgrounds)<2
+        checkIfNotDefined = len(TextureComponent.backgrounds)==0
+        check = checkLengthLess and (checkIfNotDefined or TextureComponent.backgrounds[0]!=newBackground.background)
+        if check:
+            TextureComponent.backgrounds.append(newBackground.background)
 
     def setSprites(platforms, player):
         TextureComponent.spritesB = pygame.sprite.Group()
@@ -179,72 +168,100 @@ class PhysicsComponent:
         self.is_on_ground = False
 
     def check_colision(self, moved_by_vec, other_entities = []):
+        check = False
+        def iEq0(col_entity, pos_to_check, temp_moved_vec):
+            if col_entity.type=="plat" or col_entity.type=="ladder":
+                if self.speed.y>0 and pos_to_check.y<=col_entity.pos.y-col_entity.get_height():
+                    self.speed.y = 0
+                    self.is_on_ground = True
+                    self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y - self.entity.get_height()))
+                elif col_entity.type=="ladder":
+                    self.speed.y = -7
+            else:
+                self.speed.y = 0
+                if temp_moved_vec.y > 0:
+                    self.is_on_ground = True
+                    self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y - self.entity.get_height()))
+                else:
+                    self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y + col_entity.get_height()))
+        def iEq1(col_entity, temp_moved_vec):
+            if col_entity.type!="plat" and col_entity.type!="ladder":
+                self.speed.x = 0
+                if temp_moved_vec.x > 0:
+                    self.entity.move_to_pos(vector2d(col_entity.pos.x - self.entity.get_width(), self.entity.pos.y))
+                else:
+                    self.entity.move_to_pos(vector2d(col_entity.pos.x + col_entity.get_width(), self.entity.pos.y))
+        def doorCheck(col_entity):
+            check = False
+            if col_entity.type=="door":
+                check = self.entity.useKey(col_entity.keyColor)
+                if check:
+                    col_entity.type = "decor"
+                    col_entity.WIDTH = 80
+                    col_entity.pos.x+=2
+                    col_entity.changeSprite("drzwi_" + col_entity.keyColor + ".png")
+            return check
+        def checkIfPlayer(col_entity):
+            check = True
+            removeFlag = False
+            if col_entity.type=="key":
+                self.entity.getKey(col_entity.keyColor)
+                other_entities.remove(col_entity)
+                removeFlag=True
+            elif col_entity.type=="background":
+                TextureComponent.changeBackground(col_entity)
+            elif col_entity.type=="trigger":
+                if col_entity.triggerType=="messageBox" and not col_entity.triggered:
+                    SystemComponent.showMessage(col_entity.triggerInfo)
+                    col_entity.triggered = True
+            else:
+                check = doorCheck(col_entity)
+            return [removeFlag,check]
+        def mainCollisionHandler(col_entity):
+            removeFlag = False
+            temp_moved_vec = vector2d(moved_by_vec.x, moved_by_vec.y)
+            pos_to_check = vector2d(self.entity.pos.x,self.entity.pos.y)
+            if i == 0:
+                #checks only y axis
+                pos_to_check.x -= temp_moved_vec.x
+                temp_moved_vec.x = 0
+            elif i == 1:
+                #checks only x axis
+                pos_to_check.y -= temp_moved_vec.y
+                temp_moved_vec.y = 0
+            upperBound = pos_to_check.y > col_entity.pos.y - self.entity.get_height()
+            lowerBound = pos_to_check.y < col_entity.pos.y + col_entity.get_height()
+            rightBound = pos_to_check.x + self.entity.get_width()> col_entity.pos.x
+            leftBound = pos_to_check.x < col_entity.pos.x + col_entity.get_width()
+            if upperBound and lowerBound and rightBound and leftBound:
+                check = self.entity.type=="player"
+                if check:
+                    temp = checkIfPlayer(col_entity)
+                    removeFlag = temp[0]
+                    check = temp[1]
+                if not check:
+                    if col_entity.type=="key" or col_entity.type=="background" or col_entity.type=="trigger":
+                        pass
+                    elif i == 0:
+                        iEq0(col_entity, pos_to_check, temp_moved_vec)
+                    elif i == 1:
+                        iEq1(col_entity, temp_moved_vec)
+            return removeFlag
         #collision detection and handling
         #handling is temp need to add more checks later
         self.is_on_ground = False
         for col_entity in other_entities:
-            if col_entity.type=="decor" or col_entity.type=="spawn" or col_entity.type=="spawnE" or (self.entity.type=="enemy" and col_entity.type=="enemy"):
+            decor = col_entity.type=="decor"
+            spawn = col_entity.type=="spawn" or col_entity.type=="spawnE"
+            selfColliding = (self.entity.type=="enemy" and col_entity.type=="enemy")
+
+            if decor or spawn or selfColliding:
                 pass
             else:
+                removeFlag = False
                 for i in range(2):
-                    temp_moved_vec = vector2d(moved_by_vec.x, moved_by_vec.y)
-                    pos_to_check = vector2d(self.entity.pos.x,self.entity.pos.y)
-                    if i == 0:
-                        #checks only y axis
-                        pos_to_check.x -= temp_moved_vec.x
-                        temp_moved_vec.x = 0
-                    elif i == 1:
-                        #checks only x axis
-                        pos_to_check.y -= temp_moved_vec.y
-                        temp_moved_vec.y = 0
-                    upperBound = pos_to_check.y > col_entity.pos.y - self.entity.get_height()
-                    lowerBound = pos_to_check.y < col_entity.pos.y + col_entity.get_height()
-                    rightBound = pos_to_check.x + self.entity.get_width()> col_entity.pos.x
-                    leftBound = pos_to_check.x < col_entity.pos.x + col_entity.get_width()
-                    if upperBound and lowerBound:
-                        if rightBound and leftBound:
-                            if col_entity.type=="key":
-                                self.entity.getKey(col_entity.keyColor)
-                                other_entities.remove(col_entity)
-                                break
-                            elif col_entity.type=="background":
-                                TextureComponent.changeBackground(col_entity)
-                            elif col_entity.type=="trigger":
-                                if col_entity.triggerType=="messageBox" and not col_entity.triggered:
-                                    SystemComponent.showMessage(col_entity.triggerInfo)
-                                    col_entity.triggered = True
-                            else:
-                                check = False
-                                if col_entity.type=="door":
-                                    check = self.entity.useKey(col_entity.keyColor)
-                                    if check:
-                                        col_entity.type = "decor"
-                                        col_entity.WIDTH = 80
-                                        col_entity.pos.x+=2
-                                        col_entity.changeSprite("drzwi_" + col_entity.keyColor + ".png")
-                                if not check:
-                                    if i == 0:
-                                        if col_entity.type=="plat" or col_entity.type=="ladder":
-                                            if self.speed.y>0 and pos_to_check.y<=col_entity.pos.y-col_entity.get_height():
-                                                self.speed.y = 0
-                                                self.is_on_ground = True
-                                                self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y - self.entity.get_height()))
-                                            elif col_entity.type=="ladder":
-                                                self.speed.y = -7
-                                        else:
-                                            self.speed.y = 0
-                                            if temp_moved_vec.y > 0:
-                                                self.is_on_ground = True
-                                                self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y - self.entity.get_height()))
-                                            else:
-                                                self.entity.move_to_pos(vector2d(self.entity.pos.x, col_entity.pos.y + col_entity.get_height()))
-                                    elif i == 1:
-                                        if col_entity.type!="plat" and col_entity.type!="ladder":
-                                            self.speed.x = 0
-                                            if temp_moved_vec.x > 0:
-                                                self.entity.move_to_pos(vector2d(col_entity.pos.x - self.entity.get_width(), self.entity.pos.y))
-                                            else:
-                                                self.entity.move_to_pos(vector2d(col_entity.pos.x + col_entity.get_width(), self.entity.pos.y))
+                    if not removeFlag:
+                        removeFlag = mainCollisionHandler(col_entity)
                             
 
         
@@ -314,10 +331,12 @@ class Camera:
             newOffset.x = backgroundTLCorn.x
         if check2:
             newOffset.y = backgroundBRCorn.y + window.get_height() - 80
-        if window.get_height()<TextureComponent.currentBGsize.y and (newOffset).y+backgroundTLCorn.y>0:
-            newOffset.y = backgroundTLCorn.y-80
+        if window.get_height()<=TextureComponent.currentBGsize.y+85 and (newOffset).y+-backgroundTLCorn.y>0:
+            newOffset.y = backgroundTLCorn.y
         if window.get_width()<TextureComponent.currentBGsize.x and self.camera_offset.x-backgroundBRCorn.x<360 + 0.5*(window.get_width()-800):
             newOffset.x = backgroundBRCorn.x + window.get_width() - 40
+        else:
+            newOffset.x = backgroundTLCorn.x
         self.focus_object.changeCameraOffset(newOffset)
         for entity in self.other_objects:
             entity.changeCameraOffset(newOffset)
