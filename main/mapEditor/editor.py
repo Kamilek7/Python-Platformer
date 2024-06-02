24be489b318465e38823f0b789253bf870b35dbc
kamilek7 <kamijar20031@gmail.com>
2024-06-02 12:25:43 +0200

    reprezentacja cutscenek w edytorze

--- main/mapEditor/editor.py
+++ main/mapEditor/editor.py
@@ -79,94 +79,9 @@ class Trigger(Box):
    def specialWindow(self,filewin):
       label1 = Label(filewin, text="Configure cutscene")
       actions = Listbox(filewin)
-      trigTypes = [i["type"] for i in self.cutsceneInfo]
+      trigTypes = [str(i+1) + self.cutsceneInfo[i]["type"] for i in range (len(self.cutsceneInfo))]
       for i in range(len(trigTypes)):
          actions.insert(i, trigTypes[i])
-      def idk():
-      #    if selection=="messageBox":
-      #       label2 = Label(nextWindow, text="Enter messageBox text")
-      #       text = Text(nextWindow, height = 5, width = 20)
-      #       if self.actionSpecs!=None:
-      #          text.insert(END,self.actionSpecs["text"])
-      #       spriters = listdir(AVATARS_DIR)
-      #       list2 = Listbox(nextWindow)
-      #       for i in range(len(spriters)):
-      #          list2.insert(i, spriters[i])
-      #       def idk2():
-      #          selection2 = None
-      #          for i in list2.curselection():
-      #             selection2 = list2.get(i)
-      #          self.actionSpecs = {"text" : text.get("1.0", "end-1c"), "icon" : selection2}
-      #          nextWindow.destroy()
-      #          filewin.destroy()
-      #       button2 = Button(nextWindow,text="Confirm",command=idk2)
-      #       label2.pack()
-      #       text.pack()
-      #       list2.pack()
-      #       button2.pack()
-      #    else:
-      #       def decompress(tekst):
-      #          temp=""
-      #          num = ""
-      #          for i in tekst:
-      #             if i.isdigit():
-      #                num+= i
-      #             else:
-      #                temp+= int(num)*i
-      #                num=""
-      #          return temp
-      #       label2 = Label(nextWindow, text="Enter entity id:")
-      #       id = Text(nextWindow, height=1, width=10)
-      #       labelMov = Label(nextWindow, text="Current movement:\nNone")
-      #       if self.actionSpecs!=None:
-      #          temp = decompress(self.actionSpecs["movement"])
-      #          temp1 = ""
-      #          for i in temp:
-      #             if i=="L":
-      #                temp1+="Left\n"
-      #             elif i=="R":
-      #                temp1+="Right\n"
-      #          labelMov.config(text="Current movement:\n" + temp1)
-      #          id.insert(END,self.actionSpecs["id"])
-      #       move = []
-      #       def idk2(direction,move):
-      #          move.append(direction)
-      #          if labelMov.cget("text") == "Current movement:\nNone":
-      #             labelMov.config(text="Current movement:\n" + direction + "\n")
-      #          else:
-      #             labelMov.config(text=labelMov.cget("text")+ direction + "\n")
-            
-      #       buttonLeft = Button(nextWindow, text="Move entity 1 block left",command=lambda: idk2("Left",move))
-      #       buttonRight = Button(nextWindow, text="Move entity 1 block left",command=lambda: idk2("Right",move))
-      #       def end():
-      #          def compress(tekst):
-      #             temp = ""
-      #             dig = 0
-      #             while dig<len(tekst):
-      #                num = 0
-      #                letter = tekst[dig]
-      #                while (tekst[dig+num:len(tekst)].find(letter))==(tekst[dig+num+1:len(tekst)].find(letter)):
-      #                      num+=1
-      #                num+=1
-      #                dig+=num
-      #                temp+= (str(num) + letter)
-      #             return temp
-      #          temp = ""
-      #          for i in move:
-      #             if i=="Left":
-      #                temp+="L"
-      #             elif i=="Right":
-      #                temp+="R"
-      #          temp = {"movement":compress(temp), "id": id.get("1.0", "end-1c")}
-      #          self.actionSpecs = temp
-      #       buttonEnd = Button(nextWindow, text="Save", command=end)
-      #       label2.pack()
-      #       id.pack()
-      #       labelMov.pack()
-      #       buttonLeft.pack()
-      #       buttonRight.pack()
-      #       buttonEnd.pack()
-         pass
       def showActionTypes(editFlag):
          nextWindow = Toplevel(root)
          actionsType = ["messageBox", "moveEntity"]
@@ -176,19 +91,143 @@ class Trigger(Box):
             actionTypes.insert(i, actionsType[i])
          selectionType = "messageBox"
          if editFlag:
-            for i in range (len(actionTypes.curselection())):
-               selectionType = self.cutsceneInfo[i]
-               selectionID = i
+            for i in actions.curselection():
+               id = int(actions.get(i)[0])-1
+               selectionType = self.cutsceneInfo[id]
+               selectionID = id
             selectionType = selectionType["type"]
          actionTypes.select_set(actionsType.index(selectionType))
          def getToEditWindow(editflag):
             nextWindowNext = Toplevel(root)
             selection = {}
+            movement_or_text = ""
+            icon_or_id = 0
+            delay = 0
+            for i in actionTypes.curselection():
+               selectionType = actionTypes.get(i)
             if editFlag:
                selection = self.cutsceneInfo[selectionID]
                if selectionType==selection["type"]:
-                  pass
-               
+                  values = list(selection.values())
+                  movement_or_text = values[1]
+                  icon_or_id = values[2]
+                  delay = values[3]
+               else:
+                  selection = {}
+            delayText = Label(nextWindowNext,text="Enter action delay")
+            delayT = Text(nextWindowNext,height=1, width=5)
+            delayT.insert(END, delay)
+            if selectionType=="messageBox":
+               label2 = Label(nextWindowNext, text="Enter messageBox text")
+               text = Text(nextWindowNext, height = 5, width = 20)
+               text.insert(END,movement_or_text)
+               spriteLabel = Label(nextWindowNext, text="Select avatar icon")
+               spriters = listdir(AVATARS_DIR)
+               spriters.append("None")
+               list2 = Listbox(nextWindowNext)
+               for i in range(len(spriters)):
+                  list2.insert(i, spriters[i])
+               if icon_or_id!=0:
+                  icon_or_id = spriters.index(icon_or_id)
+               list2.select_set(icon_or_id)
+               def pushInfo():
+                  selection2 = "None"
+                  for i in list2.curselection():
+                     selection2 = list2.get(i)
+                  selection = {"type": selectionType,"text" : text.get("1.0", "end-1c"), "icon" : selection2, "delay": delayT.get("1.0", "end-1c")}
+                  if editFlag:
+                     self.cutsceneInfo[selectionID] = selection
+                  else:
+                     self.cutsceneInfo.append(selection)
+                  nextWindow.destroy()
+                  nextWindowNext.destroy()
+                  filewin.destroy()
+               button3 = Button(nextWindowNext,text="Confirm",command=pushInfo)
+               label2.pack()
+               text.pack()
+               spriteLabel.pack()
+               list2.pack()
+               delayText.pack()
+               delayT.pack()
+               button3.pack()
+            elif selectionType=="moveEntity":
+               def decompress(tekst):
+                  temp=""
+                  num = ""
+                  for i in tekst:
+                     if i.isdigit():
+                        num+= i
+                     else:
+                        temp+= int(num)*i
+                        num=""
+                  return temp
+               label2 = Label(nextWindowNext, text="Enter entity id:")
+               id = Text(nextWindowNext, height=1, width=10)
+               id.insert(END,icon_or_id)
+               labelMov = Label(nextWindowNext, text="Current movement:\nNone")
+               move = []
+               if movement_or_text!="":
+                  temp = decompress(movement_or_text)
+                  temp1 = ""
+                  for i in temp:
+                     if i=="L":
+                        temp1+="Left\n"
+                        move.append("Left")
+                     elif i=="R":
+                        temp1+="Right\n"
+                        move.append("Right")
+                     else:
+                        temp1+="Wait\n"
+                        move.append("Wait")
+                  labelMov.config(text="Current movement:\n" + temp1)
+               def createMovement(direction,move):
+                  move.append(direction)
+                  if labelMov.cget("text") == "Current movement:\nNone":
+                     labelMov.config(text="Current movement:\n" + direction + "\n")
+                  else:
+                     labelMov.config(text=labelMov.cget("text")+ direction + "\n")
+               buttonLeft = Button(nextWindowNext, text="Move entity 1 block left",command=lambda: createMovement("Left",move))
+               buttonStop = Button(nextWindowNext, text="Make entity wait",command=lambda: createMovement("Wait",move))
+               buttonRight = Button(nextWindowNext, text="Move entity 1 block left",command=lambda: createMovement("Right",move))
+               def end():
+                  def compress(tekst):
+                     temp = ""
+                     dig = 0
+                     while dig<len(tekst):
+                        num = 0
+                        letter = tekst[dig]
+                        while (tekst[dig+num:len(tekst)].find(letter))==(tekst[dig+num+1:len(tekst)].find(letter)):
+                              num+=1
+                        num+=1
+                        dig+=num
+                        temp+= (str(num) + letter)
+                     return temp
+                  temp = ""
+                  for i in move:
+                     if i=="Left":
+                        temp+="L"
+                     elif i=="Right":
+                        temp+="R"
+                     else:
+                        temp+="W"
+                  temp = {"type": "moveEntity","movement":compress(temp), "id": id.get("1.0", "end-1c"),"delay": delayT.get("1.0","end-1c")}
+                  if editFlag:
+                     self.cutsceneInfo[selectionID] = temp
+                  else:
+                     self.cutsceneInfo.append(temp)
+                  nextWindow.destroy()
+                  nextWindowNext.destroy()
+                  filewin.destroy()
+               buttonEnd = Button(nextWindowNext, text="Save", command=end)
+               label2.pack()
+               id.pack()
+               labelMov.pack()
+               buttonLeft.pack()
+               buttonStop.pack()
+               buttonRight.pack()
+               delayText.pack()
+               delayT.pack()
+               buttonEnd.pack()
          button = Button(nextWindow, text="Proceed", command=lambda: getToEditWindow(editFlag))
          actionTypes.pack()
          button.pack()
@@ -452,8 +491,7 @@ def loadfile(_filename,filewin):
          if child.tagName=="background":
             grounds.append(Background(int(child.getAttribute("x")),int(child.getAttribute("y")),int(child.getAttribute("width")),int(child.getAttribute("height")), background=child.getAttribute("background")))
          elif child.tagName=="trigger":
-            specs = eval(child.getAttribute("actionSpecs"))
-            grounds.append(Trigger(int(child.getAttribute("x")),int(child.getAttribute("y")),int(child.getAttribute("width")),int(child.getAttribute("height")), actionSpecs=specs,actionType=child.getAttribute("actionType")))
+            grounds.append(Trigger(int(child.getAttribute("x")),int(child.getAttribute("y")),int(child.getAttribute("width")),int(child.getAttribute("height")), cutsceneInfo=child.getAttribute("cutsceneInfo")))
             
          elif child.tagName=="enemy":
             grounds.append(EnemyPlaceholder(int(child.getAttribute("x")),int(child.getAttribute("y")), child.getAttribute("type"), id=child.getAttribute("id")))
